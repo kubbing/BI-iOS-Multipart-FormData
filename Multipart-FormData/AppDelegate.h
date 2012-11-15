2f0b189b5d8e9ec3741f699e5b3979e86999f214
Jakub Hladik <kubbing@me.com>
2012-11-15 19:10:51 +0100

    Initial Commit

---
+++ Multipart-FormData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multipart-FormData
+//
+//  Created by Jakub Hladík on 15.11.12.
+//  Copyright (c) 2012 Jakub Hladík. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
